7e8488b485db506c37b0ef4a8a36a35104952f44
carmenfaye28 <carmenfaye28@gmail.com>
2021-08-03 19:47:32 -0500

    set up halfway done

---
+++ js/app.js
@@ -0,0 +1,32 @@
+class Tamagotchi { 
+    constructor(name) {
+        this.name = name
+        this.owner = this.owner
+        this.hunger = 0
+        this.boredom = 0
+        this.sleepy = 0
+        this.age = 0
+        this.bank = 0
+    }
+
+    food = []
+    toy = []
+
+
+
+    eatFood() {
+        // show pet bouncing up and down
+        //decrease hunger by 1
+    
+    }
+
+    playToy () {
+        //show pet moving side to side
+        
+    }
+
+    lightsOut() {
+
+    }
+
+}
